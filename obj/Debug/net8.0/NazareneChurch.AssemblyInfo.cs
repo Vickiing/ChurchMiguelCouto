500f892316b94c6a343c651df9587f5afea59a81
Victor Silva <vlsilva@zsul.onmicrosoft.com>
2024-07-27 18:34:27 -0300

    refact: mudança de Descricoes na pagina Index

--- obj/Debug/net8.0/NazareneChurch.AssemblyInfo.cs
+++ obj/Debug/net8.0/NazareneChurch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NazareneChurch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793a46316b1d83c0b7b35b12d892f62428d0cd6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ac91c84de10ea7ab587f0585106b27c8f16502")]
 [assembly: System.Reflection.AssemblyProductAttribute("NazareneChurch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NazareneChurch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
